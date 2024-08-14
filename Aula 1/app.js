2df3a1d015d348672bec6345501f057235968af3
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-14 16:53:51 -0300

    Revisando HMTL

---
+++ Aula 1/app.js
@@ -0,0 +1,12 @@
+let idade = 20
+let anoAtual = 2024
+console.log(idade, anoAtual)
+
+idade = 21
+console.log(idade)
+
+const pontos = 100
+console.log(pontos)
+
+var score = 150 // não e mais tão utilizada a var foi substituida por 'const' e 'let'
+console.log(score) 
