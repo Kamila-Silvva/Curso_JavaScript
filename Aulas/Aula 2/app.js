3c6b1fc0c4eaa72d587e33d37e1ce099c68a00ca
Kamila-Silvva <kamilaalvesdemiranda@gmail.com>
2024-08-16 16:40:25 -0300

    Até a aula 10

---
+++ Aulas/Aula 2/app.js
@@ -0,0 +1,31 @@
+console.log('Kamila')
+
+const email = 'kamilaalvesdemiranda@gmail.com'
+
+console.log(email)
+
+const nome = 'Kamila'
+const sobrenome = 'Silva'
+const nomeSobrenome = nome + ' ' + sobrenome // contatenando constantes
+
+console.log(nomeSobrenome); // Imprimindo constantes no console
+
+console.log(nomeSobrenome[3]) // Acessando os caractéres
+
+console.log(nomeSobrenome.length); // Exibindo o tamanho das caractéres
+
+
+// Função = pedaços de código que executa alguma ação específica
+// Método = é uma função que está associada a um objeto ou tipo de dado em particula
+// Em essencia as duas palavras tem o mesmo significado
+
+console.log(nomeSobrenome.toUpperCase()) // Usado para colocar todas as caracteres em maiúsculo
+
+const resultado = nomeSobrenome.toLowerCase() //faz com que todos os caractes ficam minúsculo
+
+console.log(resultado);
+
+const index = email.indexOf('@') //encontra a posição que vai ser especificada dentros dos parenteses
+
+console.log(index);
+
